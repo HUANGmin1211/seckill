be000697d285e6b1cfaeb34726d47060dd3a2157
HUANGmin1211 <1002928984@qq.com>
2019-06-14 16:26:36 +0800

    Initial commit

---
+++ src/main/java/com/hm/seckill/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.hm.seckill.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String home(){
+        return "hello world";
+    }
+}
